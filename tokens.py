e1b0f5f6a0d6af2b8c57f3fb6670471c5631604f
“Fraser <2192722g@student.gla.ac.uk>
2017-02-21 12:18:20 +0000

    access token

--- tokens.py
+++ tokens.py
@@ -3,7 +3,7 @@ import os
 
 # database
 
-def urltodb(url):
+def urltodb(url):#url = DATABASE_URL
 	url = url.replace('//','').replace('@',':').split(':')
 	url[-1] = url[-1][url[-1].index('/')+1:]
 	return web.database(dbn=url[0],db=url[4],user=url[1],pw=url[2],host=url[3])
@@ -23,6 +23,7 @@ else:
 webhook = 'kjhvvjhkvkgCGHgCJHjghcY6i7Cc7tt'
 page = {
 	'SecretTestBot':'EAAFuDsxVFzwBANcbN1E5J4tgdwIYk2bDNYkQ5gSw11RtVNfFq5F5z5xCXG1OauqWnMrMHpi3EZCUDJenqoZBKs0VugcVwRCdeWiAws5WfedR2EfLUomSqfXeMfxQLrf2so0iGWkwlaU5XHEEITw8bdzayykNhuLSzRZC6933QZDZD',
+	'SecretBotLiveTest':'EAAFuDsxVFzwBAF7y9LVZBwwKREkq4SxZCPXxCxFytB4KAZCDZCnvnpuYL4ne1aVjffyJjOaZB9yKwRgqrAqEDtZCq6D48E7pNffMNFNWzcW6Ih3i48bCrhCZCobvZCqjCnGZB6vNyYWZB2E0zaiKANcipYL4WwDCHvN8R818sSZCFZCTKAZDZD',
 	'SecretBot':'EAAae68efi70BAPQUsusZCkfTm1KMRfETnLU3207hUMI5rywtFeKGe6plb7qpWybhKyVsu306fYC8ZA7fG9p7Mg4qi67KkP2XDqJ8ZAkUDg6ZCTYSDBiRQN8ZC48xcyV8voal8pXMyrg9VIfKtjiSEljYZC3QvsC11ZAwiJ7hXaKRQZDZD'
 }
 
@@ -32,4 +33,5 @@ webhook_key = webhook
 if TEST:
 	access_token = page['SecretTestBot']
 else:
-	access_token = page['SecretBot']
+	access_token = page['SecretBotLiveTest']
+	#access_token = page['SecretBot']
